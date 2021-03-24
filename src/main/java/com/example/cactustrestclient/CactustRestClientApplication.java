1f159da989a0f8f8f1cad9ca63cc7872f5a377a5
muneeb706 <muneeb706@gmail.com>
2021-03-24 12:28:05 -0500

    spring boot application template

---
+++ src/main/java/com/example/cactustrestclient/CactustRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.cactustrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactustRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CactustRestClientApplication.class, args);
+	}
+
+}
